84ff3674a466473d3ed8aa1f70293baf4987f298
Varun Teja Maguluri <98674002+VarunTejaMaguluri0090@users.noreply.github.com>
2023-04-19 14:37:36 -0700

    Add files via upload

---
+++ backend/pool.js
@@ -0,0 +1,36 @@
+//imported mysql
+const mysql = require('mysql');
+
+//created object for mysql + add settings
+const connection = mysql.createConnection({
+    host     :  'localhost',
+    user     : 'root',
+    password : 'root1234',
+    port : 3306,
+    database : 'cloud281'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  else{
+    console.log('Connected!');
+}
+ 
+});
+module.exports = connection;
+
+
+// https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+// https://apple.stackexchange.com/questions/176786/how-to-add-mysql-to-path-variable-to-resolve-mysql-command-not-found
+
+// to start sql in terminal
+
+// mysql -u root -p
+
+// if mysql command not found: 
+// export PATH=${PATH}:/usr/local/mysql/bin
+// In the terminal - show databases, use Cloud281 -> show tables;
